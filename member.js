0061dc192e2f3b3560fce356259c3b33d82d9fbf
Zlash01 <97034441+Zlash01@users.noreply.github.com>
2024-08-23 03:06:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillMembers(){
+    return [
+        {name: 'A', skill: 'JavaScript'},
+        {name: 'B', skill: 'JavaScript'},
+        {name: 'C', skill: 'Java'},
+        {name: 'D', skill: 'Java'},
+        {name: 'E', skill: 'Python'},
+        {name: 'F', skill: 'Python'},
+    ];
+}
